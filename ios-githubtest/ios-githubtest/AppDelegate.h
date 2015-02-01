c268b153a18f97bee4f247b205afcfa33b47b960
love <bailei1234567@gmail.com>
2015-02-01 19:50:50 +0800

    this is a test

---
+++ ios-githubtest/ios-githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-githubtest
+//
+//  Created by love on 15/2/1.
+//  Copyright (c) 2015年 bldmm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
